1b082a3689cc64ea120732aa8608421ce0f8212d
Garcia <Jafit.Garcia@sovos.com>
2022-06-02 13:02:38 -0500

    Se agrega proyecto

---
+++ src/test/java/com/example/demo1/MainPageTest.java
@@ -0,0 +1,39 @@
+package com.example.demo1;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import java.util.concurrent.TimeUnit;
+
+public class MainPageTest {
+
+
+    @BeforeAll
+    public static void setUp() {
+
+    }
+
+    @AfterEach
+    public void tearDown() {
+
+
+    }
+
+    @Test
+    public void search() {
+
+    }
+
+    @Test
+    public void toolsMenu() {
+    }
+
+
+}
